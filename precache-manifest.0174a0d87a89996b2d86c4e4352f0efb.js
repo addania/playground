a9eefcdabef8c6d5e1336cd230d6e95d0d1cff02
addania <mirka.schw@gmail.com>
2019-11-01 16:20:00 +0100

    Updates

---
+++ precache-manifest.0174a0d87a89996b2d86c4e4352f0efb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b362fb7b4c09f87e056e62a9677d88",
+    "url": "/playground/index.html"
+  },
+  {
+    "revision": "50b89aaa7111919536b6",
+    "url": "/playground/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ce95311e8ec3e0727d2c",
+    "url": "/playground/static/js/2.52c03e6f.chunk.js"
+  },
+  {
+    "revision": "50b89aaa7111919536b6",
+    "url": "/playground/static/js/main.9bb35ab4.chunk.js"
+  },
+  {
+    "revision": "9722095d447a9c358dcd",
+    "url": "/playground/static/js/runtime-main.51723f05.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/playground/static/media/logo.25bf045c.svg"
+  }
+]);
